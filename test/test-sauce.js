e177e5bec7ff471bfb158c6542cef4dd07f0148c
tiaratrisanti <tiara.trisaramadhani@gmail.com>
2025-12-25 09:36:00 +0700

    assingments: Web UI Automation Fundamental

--- test/test-sauce.js
+++ test/test-sauce.js
@@ -1,35 +1,24 @@
-const { Builder, By, until } = require('selenium-webdriver');
-const { expect } = require('chai');
+const { Builder, By } = require('selenium-webdriver');
+const assert = require('assert');
 
-describe('Tugas Automate Saucedemo', function() {
-    this.timeout(30000); //
+describe('Tugas Sesi 9 - Web UI Automation Fundamental', function () {
+    this.timeout(60000);
     let driver;
 
-
-    before(async function() {
+    it('Login dan Sortir A-Z (Gaya Fundamental)', async function () {
         driver = await new Builder().forBrowser('chrome').build();
-    });
-
-
-    after(async function() {
-        await driver.quit();
-    });
-
-    it('Scenario 1: Sukses Login', async function() {
+        
         await driver.get('https://www.saucedemo.com/');
         await driver.findElement(By.id('user-name')).sendKeys('standard_user');
         await driver.findElement(By.id('password')).sendKeys('secret_sauce');
         await driver.findElement(By.id('login-button')).click();
-
-        let title = await driver.findElement(By.className('title')).getText();
-        expect(title).to.equal('Products');
-    });
-
-    it('Scenario 2: Urutkan Produk dari A-Z', async function() {
-        const dropdown = await driver.findElement(By.className('product_sort_container'));
-        await dropdown.click();
+        
+        await driver.findElement(By.className('product_sort_container')).click();
         await driver.findElement(By.xpath("//option[@value='az']")).click();
-        let firstItemName = await driver.findElement(By.className('inventory_item_name')).getText();
-        expect(firstItemName).to.equal('Sauce Labs Backpack');
+
+        let firstItem = await driver.findElement(By.className('inventory_item_name')).getText();
+        assert.strictEqual(firstItem, 'Sauce Labs Backpack');
+
+        await driver.quit();
     });
 });
